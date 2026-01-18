5a15152093987b294b184de38394e454cda5bcca
Pau Mas <pmasduran@gmail.com>
2026-01-18 19:06:23 +0100

    Chapter 8 before Onchange

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -1,5 +1,5 @@
 
-from odoo import fields, models # type: ignore
+from odoo import fields, models, api # type: ignore
 
 class estateProperty(models.Model):
     _name = "estate.property"
@@ -11,7 +11,7 @@ class estateProperty(models.Model):
     postcode = fields.Char()
     date_availability = fields.Date(string= "Available From", copy=False, default=lambda self: fields.Date.add(fields.Date.context_today(self),months=3))
     expected_price = fields.Float(required=True)
-    selling_price = fields.Float(copy=False, readonly=True)
+    selling_price = fields.Float(copy=False, readonly=True, compute="_best_offer")
     bedrooms = fields.Integer(default="2")
     living_area = fields.Integer()
     facades = fields.Integer()
@@ -26,9 +26,29 @@ class estateProperty(models.Model):
                             required= True, 
                             default="new")
     active = fields.Boolean(string="Active", default=True)
+
+
     # foreign keys
     property_type_id = fields.Many2one('estate.property.type', string="Property Type")
     buyer_id = fields.Many2one('res.partner', string="Buyer", copy=False)
     salesman_id = fields.Many2one('res.users', string="Salesman")
     tag_id = fields.Many2many('estate.property.tag', string="Tags")
     offer_ids = fields.One2many('estate.property.offer', 'property_id', string="Offers")
+
+
+    #Computed Fields
+    total_area = fields.Float(string="Total Area", compute="_compute_total_area")
+
+    @api.depends("living_area","garden_area")
+    def _compute_total_area(self):
+        for record in self:
+            record.total_area = record.living_area + record.garden_area
+
+    @api.depends("offer_ids.price")
+    def _best_offer(self):
+        
+        for record in self:
+            prices = record.offer_ids.mapped("price")
+            record.selling_price = max(prices) if prices else 0
+
+    
--- estate/models/estate_property_offer.py
+++ estate/models/estate_property_offer.py
@@ -1,4 +1,4 @@
-from odoo import fields, models # type: ignore
+from odoo import fields, models, api # type: ignore
 
 class estatePropertyOffer (models.Model):
     _name = "estate.property.offer"
@@ -8,3 +8,31 @@ class estatePropertyOffer (models.Model):
     status = fields.Selection(copy = False, selection= [("accepted","Accepted"),("refused","Refused")])
     partner_id = fields.Many2one('res.partner', required=True)
     property_id = fields.Many2one('estate.property',required=True)
+    validity = fields.Integer(default=7)
+    date_deadline = fields.Date(compute="_compute_date_deadline", inverse="_inverse_date_deadline", store=True)
+
+    @api.depends("create_date","validity")
+    def _compute_date_deadline(self):
+        
+        for record in self:
+            base_date = (
+                record.create_date.date()
+                if record.create_date
+                else fields.Date.context_today(record)
+            )
+            record.date_deadline = fields.Date.add(
+                base_date,
+                days=record.validity
+            )
+
+    def _inverse_date_deadline(self):
+        for record in self:
+            if record.date_deadline:
+                base_date = (
+                    record.create_date.date()
+                    if record.create_date
+                    else fields.Date.context_today(record)
+                )
+                record.validity = (record.date_deadline - base_date).days
+            else:
+                record.validity = 7
